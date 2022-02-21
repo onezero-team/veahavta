fe39137bf749660d6b81a1fb2cd2e68f87fc2b00
Galor-sw <galor546@gmail.com>
2022-02-22 00:05:44 +0200

    section what we do part 2

--- components/HomeComponents/whatwedo.tsx
+++ components/HomeComponents/whatwedo.tsx
@@ -4,23 +4,28 @@ import { PageType } from '../Types'
 import Card from '../card'
 
 export default function WhatWeDo({ data }: PageType) {
-    return (
-        <>
-            <section className="grid place-items-center">
-                <div className="text-2xl text-[#D25C78] text-center">
-                    {data.homepage.whatWeDoHeading}
-                </div>
-                <h1 className="text-[82px] text-center">
-                    {data.homepage.whatWeDoTitle}
-                </h1>
-                <div className="grid grid-cols-4 place-items-center">
-                    {data.homepage.whatWeDoCards.map((card, i) => {
-                        return (
-                            <Card title={card.title} text={card.text} imagePath={card.imagePath} key={i} />
-                        )
-                    })}
-                </div>
-            </section>
-        </>
-    )
+  return (
+    <>
+      <section className="grid place-items-center">
+        <div className="text-2xl text-[#D25C78] text-center">
+          {data.homepage.whatWeDoHeading}
+        </div>
+        <h1 className="text-[82px] text-center">
+          {data.homepage.whatWeDoTitle}
+        </h1>
+        <div className="grid grid-cols-4 place-items-center">
+          {data.homepage.whatWeDoCards.map((card, i) => {
+            return (
+              <Card
+                title={card.title}
+                text={card.text}
+                imagePath={card.imagePath}
+                key={i}
+              />
+            )
+          })}
+        </div>
+      </section>
+    </>
+  )
 }
