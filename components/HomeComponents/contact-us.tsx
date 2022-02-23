c1c4f60af6e593c0541744293877324d83c03ac7
Galor-sw <galor546@gmail.com>
2022-02-23 10:25:56 +0200

    attached section about-us, attached section- contact-us

--- components/HomeComponents/contact-us.tsx
+++ components/HomeComponents/contact-us.tsx
@@ -1,34 +1,21 @@
 import React from 'react'
 import { PageType } from '../Types'
+import ContactData from '../contactdata'
 
 export default function ContactUs({ data }: PageType) {
   return (
     <>
-      <section className="grid grid-cols-2">
-        <div>
+      <section className="grid grid-cols-2 place-items-center mt-64">
+        <div className="grid place-items-center">
           <h2>{data.homepage.contactUsHeading}</h2>
-          <h2>{data.homepage.contactUsTitle}בשבילכם</h2>
-          <div className="w-5/12 grid grid-cols-2">
+          <h2  className="text-4xl font-bold">{data.homepage.contactUsTitle}בשבילכם</h2>
+          <p className="w-4/12">{data.homepage.contactUsText}</p>
+          <div className=" grid grid-cols-2 place-items-center">
             {data.common.contactUsLinks.map((i, key) => {
-              let Type: string;
-              if (i.linkType === 'address') {
-                Type = `https://www.google.co.il/maps/search/` + `${i.text}`;
-              }
-              else {
-                Type = (i.linkType === "tel" ? 'tel:' : 'mailto')
-              }
               return (
-                <div className="w-8/12" key={key}>
-                  <a type={i.linkType}
-                    href={`${Type}`} key={key}>
-                    {i.text}
-                  </a>
-
-                </div>
+                <ContactData linkType={i.linkType} text={i.text} imagePath={i.imagePath} key={key}/>
               )
-            }
-            )
-            }
+            })}
           </div>
         </div>
       </section>
--- components/text.tsx
+++ components/text.tsx
@@ -1,3 +1,3 @@
 export const PageHeader = ({ children }: { children: string }) => {
-  return <h1 className="w-4/12 text-center text-[82px] pt-24">{children}</h1>
+  return <h1 className="w-4/12 text-center text-[82px] pt-24 font-bold">{children}</h1>
 }
